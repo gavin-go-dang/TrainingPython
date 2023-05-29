9cedf9b448595af13c42be297bc8e5800c4ba004
gavin-go-dang <gavin.dang.goldenowl@gmail.com>
2023-05-29 10:02:51 +0700

    exercise01

---
+++ exercise02.py
@@ -0,0 +1,8 @@
+#solution on https://www.practicepython.org/exercise/2014/02/05/02-odd-or-even.html
+#read number in file input02.txt
+#and write the result to file result.txt
+
+
+number = int(input())
+
+
